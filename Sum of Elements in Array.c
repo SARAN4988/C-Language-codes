84cd71742fb45eae7bcfaa30b31f9ae0588b2c54
SARAN4988 <saran07302006@gmail.com>
2026-02-06 14:27:37 +0530

    Create Sum of Elements in Array.c

---
+++ Sum of Elements in Array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int size;
+    scanf("%d",&size);
+    int arr[size];
+    int sum=0;
+    for(int i=0;i<size;i++){
+        scanf("%d",&arr[i]);
+        sum=sum+arr[i];
+        
+            
+    }
+    printf("%d",sum);
+    
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
